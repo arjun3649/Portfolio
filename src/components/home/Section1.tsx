4b0e2822d9bf7a838e729be5914275639cc9c483
arjun3649 <arjun.rakwal3649@gmail.com>
2024-11-06 15:01:26 +0530

    changed Section1 css

--- src/components/home/Section1.tsx
+++ src/components/home/Section1.tsx
@@ -15,8 +15,8 @@ const HomeSection1 = ({ id }: Readonly<{ id: string }>) => {
     >
       <ConstrainedBox classNames="px-4 py-8 pt-16 z-20 items-center justify-center">
         <Column classNames="w-full items-center justify-center">
-          <div className="inline-flex items-center w-full justify-center">
-            <p className="text-2xl/normal sm:text-3xl/normal md:text-5xl/normal lg:text-6xl/normal xl:text-7xl/normal dark:text-[var(--textColor)] text-[var(--textColor)] font-bold text-center">
+          <div className="inline-flex items-center ">
+            <p className="text-2xl/normal sm:text-3xl/normal md:text-5xl/normal xl:text-7xl/normal dark:text-[var(--textColor)] text-[var(--textColor)] font-bold text-center">
               Hi there, I am
             </p>
             <FlipWords
